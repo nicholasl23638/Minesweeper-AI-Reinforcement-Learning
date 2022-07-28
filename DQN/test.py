59862a870ea1ad28836b58d18f70f96af388c547
Ginget <75821392+nicholasl23638@users.noreply.github.com>
2022-07-28 15:38:28 -0500

    Update test.py

--- DQN/test.py
+++ DQN/test.py
@@ -1,7 +1,7 @@
 import argparse
 from tqdm import tqdm
 from keras.models import load_model
-from MinesweeperAgentWeb import *
+import MinesweeperAgentWeb
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Play Minesweeper online using a DQN')
